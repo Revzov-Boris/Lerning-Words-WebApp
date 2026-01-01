6779f3b9ef045cd95a618db69544e7e4b79f9a6a
Boris Revzov <boria.revazov@yandex.ru>
2026-01-02 00:15:18 +0300

    улучшена валидация языков

--- src/main/java/com/example/learning_words_app/dto/LanguageAddForm.java
+++ src/main/java/com/example/learning_words_app/dto/LanguageAddForm.java
@@ -1,12 +1,14 @@
 package com.example.learning_words_app.dto;
 
 import com.example.learning_words_app.utils.validation.UniqueLanguageTitle;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record LanguageAddForm(
     @NotNull(message = "Название не может быть Null")
     @NotEmpty(message = "Название не может быть пустым")
+    @NotBlank(message = "Название должно состоять из символов")
     @UniqueLanguageTitle
     String title
 ) {
