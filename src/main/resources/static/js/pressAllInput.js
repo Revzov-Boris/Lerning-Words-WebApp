e176feff7e14f1847e0441710852febfb1b3bfec
Boris Revzov <boria.revazov@yandex.ru>
2026-03-06 02:06:05 +0300

    возможность убрать все слова из тренировки
    после нажатия кнопки 'Выбрать всё' она становится кнопкой 'Вычеркнуть всё'

--- src/main/resources/static/js/pressAllInput.js
+++ src/main/resources/static/js/pressAllInput.js
@@ -1,7 +1,10 @@
 function pressAll() {
+    isAll = !isAll
     const items = document.querySelectorAll('input');
-    items.forEach(b => b.checked = true)
+    items.forEach(b => b.checked = isAll)
+    pressAllBut.textContent = isAll ? "Вычеркнуть всё" : "Выделить всё"
 }
 
+let isAll = false;
 const pressAllBut = document.getElementById("pressAllButton")
 pressAllBut.onclick = pressAll
